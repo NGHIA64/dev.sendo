5eed781cb1a3f6d52f6de6eb1253766af97fba55
nghia64 <nguyentrongn168@gmail.com>
2023-01-10 17:45:54 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Header from './Components/Header/main';
+function App() {
+  return 
+  <>
+  <Headers/>
+  </>
+}
+
+export default App;
---
+++ src/Components/Header/main.js
@@ -0,0 +1,9 @@
+import React from "react";
+class Header extends React.Component{
+    render(){
+        return <>
+        123
+        </>
+    }
+}
+export default Header
