c62b34ee463d30f83719cb2e1da6d8166bd72b8d
NGHIA64 <83792258+NGHIA64@users.noreply.github.com>
2023-01-12 03:53:22 +0700

    product

--- src/Components/Body/sidebar2.js
+++ src/Components/Body/sidebar2.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { sidebar2 } from "../../Data/Body/sidebar2";
 class Sidebar2 extends React.Component {
+   numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + 'đ';
+}
   render() {
+    console.log(sidebar2)
     return (
       <>
         <div>
@@ -12,6 +17,24 @@ class Sidebar2 extends React.Component {
             <span className="text-[#6f787e] text-[16px] font-[600]">
               Sản phẩm được tài trợ
             </span>
+            <div className="flex">
+              {sidebar2.sanPhamDuocTaiTro.map((item, index) => {
+                return (
+                  <>
+                    <div className="">
+                      <img src={item.image} />
+                      <div>
+                        <img src="https://media3.scdn.vn/img4/2020/07_30/h6fJaiL5WkEbDU2eQRZb.png" />
+                        <span>{item.name}</span>
+                      </div>
+                      <div>{this.numberWithCommas(item.final_price)}</div>
+                      <div>{item.shop_warehouse_city}</div>
+                    </div>
+                    ;
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
       </>
