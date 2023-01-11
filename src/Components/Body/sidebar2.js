fc30174bbafbc92b18c5b7fe3b71854e07764a23
NGHIA64 <83792258+NGHIA64@users.noreply.github.com>
2023-01-12 04:13:40 +0700

    done product

--- src/Components/Body/sidebar2.js
+++ src/Components/Body/sidebar2.js
@@ -36,6 +36,42 @@ class Sidebar2 extends React.Component {
               })}
             </div>
           </div>
+          <div className="flex">
+            {sidebar2.sanPhamBinhThuong.map( (item, index) => {
+                return (
+                  <>
+                    <div>
+                      <img src={item.image} />
+                      <div className="flex">
+                        <img src="https://media3.scdn.vn/img4/2022/11_09/6WTj8y7uau8dW1m4jiDA.png" />
+                        <img src="https://media3.scdn.vn/img4/2022/12_29/ZP0hPP4m4WbFPaY8mLmG.png" />
+                      </div>
+                      <div>
+                        <img src="https://media3.scdn.vn/img4/2020/07_30/h6fJaiL5WkEbDU2eQRZb.png" />
+                        <span>{item.name}</span>
+                      </div>
+                      <div className="flex">
+                        <span>{item.sale_percent + "%"}</span>
+                        <span>
+                          {this.numberWithCommas(item.default_price_max)}
+                        </span>
+                      </div>
+                      <div>{this.numberWithCommas(item.sale_price_max)}</div>
+                      <div>
+                        <img src="https://media3.scdn.vn/img4/2022/11_09/gj399CMo7lEnCO3fCiQ9.png" />
+                        <span>Trả góp 0%</span>
+                      </div>
+                      <div>Đã bán {item.order_count_text}</div>
+                      <div>
+                        {item.rated.star}/5
+                        <span class="_8511-ioQgyg"></span>
+                        <span>{item.shop.ware_house_region_name}</span>
+                      </div>
+                    </div>
+                  </>
+                );
+            })}
+          </div>
         </div>
       </>
     );
