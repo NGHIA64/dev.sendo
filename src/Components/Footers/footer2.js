796058a007b1b16935842bd04b053374ab5a15cf
nghia64 <nguyentrongn168@gmail.com>
2023-01-12 13:31:29 +0700

    done data footer 4

--- src/Components/Footers/footer2.js
+++ src/Components/Footers/footer2.js
@@ -1,7 +1,57 @@
 import React from "react";
+import { footer2 } from "../../Data/Footer/footer2";
 class Footer2 extends React.Component {
   render() {
-    return <></>;
+    console.log(footer2)
+    return (
+      <>
+        <div className="flex bg-[#f2f3f4]">
+          {footer2.map((item, index) => {
+            return (
+              <div>
+                <span>{item[0].props.children}</span>
+                <br></br>
+                {item[1].props.list.map((item, index) => {
+                  if(item.appStore){
+                    return (
+                      <>
+                        <span>{item.text}</span>
+                        <div>
+                          <div>
+                            <a>
+                              <img src={item.appStore.image} />
+                            </a>
+                          </div>
+                          <div>
+                            <a>
+                              <img src={item.googlePlay.image} />
+                            </a>
+                          </div>
+                          <div>
+                            <a>
+                              <img src={item.appGallery.image} />
+                            </a>
+                          </div>
+                        </div>
+                        <br></br>
+                      </>
+                    );
+                  }else{
+                    return (
+                      <>
+                        <a>{item.text}</a>
+                        <br></br>
+                      </>
+                    );
+                  }
+                  
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
   }
 }
 export default Footer2;
--- src/Components/Footers/footer3.js
+++ src/Components/Footers/footer3.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { footer3 } from "../../Data/Footer/footer3";
 class Footer3 extends React.Component {
+
   render() {
+    console.log(footer3)
     return <></>;
   }
 }
