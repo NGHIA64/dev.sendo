ddf6b2622d801abb9d7ac98a853ad46245c4e4ab
nghia64 <nguyentrongn168@gmail.com>
2023-01-12 16:52:13 +0700

    done data

--- src/Components/Footers/footer3.js
+++ src/Components/Footers/footer3.js
@@ -4,7 +4,69 @@ class Footer3 extends React.Component {
 
   render() {
     console.log(footer3)
-    return <></>;
+    return (
+      <>
+        <div className="text-white bg-[#0f1e29] flex">
+          <div>
+            {footer3.map((item, index) => {
+              if (item) {
+                if (item.children && typeof item.children == "string") {
+                  if (index == 0) {
+                    return (
+                      <>
+                        <div>
+                          <span className="font-[700]">{item.children}</span>
+                        </div>
+                      </>
+                    );
+                  } else {
+                    return (
+                      <>
+                        <div>
+                          <span>{item.children}</span>
+                        </div>
+                      </>
+                    );
+                  }
+                }
+              }
+            })}
+            <span>
+              Email: <a href="mailto:lienhe@sendo.vn">lienhe@sendo.vn</a>
+            </span>
+            <div className="flex">
+              <a>
+                <img src="https://media3.scdn.vn/img4/2020/12_16/XhpGDnvWqrlKeHLst3aS.png" />
+              </a>
+              <a>
+                <img src="https://media3.scdn.vn/img4/2020/12_16/h6lEMGIAt4Uapd0Mls34.png" />
+              </a>
+            </div>
+          </div>
+          <div>
+            <span>Đăng ký nhận bản tin ưu đãi khủng từ Sendo</span>
+            <form>
+              <div>
+                <div>
+                  <div>
+                    <input
+                      placeholder="Email của bạn là"
+                      type="text"
+                      inputMode="email"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <button className="bg-[#ee2624] text-[#fff]">
+                  <span>Đăng ký</span>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 export default Footer3;
--- src/Components/Footers/footer4.js
+++ src/Components/Footers/footer4.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { topTimkiem } from "../../Data/Footer/footer4"
 class Footer4 extends React.Component {
   render() {
-    return <></>;
+    console.log(topTimkiem)
+    return (
+      <>
+        <div className="flex">
+          <span className="text-[#0f1e29] font-[700] text-[12px]">TOP TÌM KIẾM</span>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            xlink="http://www.w3.org/1999/xlink"
+            class="d7ed-SwZDZ2"
+          >
+            <path
+              fill="#6F787E"
+              fill-rule="nonzero"
+              d="M12 13.214 17.524 8 19 9.393 12 16 5 9.393 6.476 8z"
+            ></path>
+          </svg>
+        </div>
+        <div>
+          {topTimkiem.list.map((item, index) => {
+            return (
+              <>
+                <a className="text-[#0f62fe] font-[400] text-[12px]">
+                  {item.name}
+                </a>
+              </>
+            );
+          })}
+        </div>
+      </>
+    );
   }
 }
 export default Footer4;
